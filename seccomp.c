80f2ffb66fea09cbb1077e365388cee8ed575ea8
panoskostopoulos <panoskostopoulos@gmail.com>
2025-07-11 15:42:01 +0300

    fix identation cause by editor

--- seccomp.c
+++ seccomp.c
@@ -46,15 +46,15 @@ FILE* sc_must_read_and_validate_header_from_file(const char *profile_path, struc
 
 void sc_must_read_filter_from_file(FILE *file, uint32_t len_bytes, struct sock_fprog *prog)
 {
-    if (len_bytes == 0){
-        die("len_bytes can not be 0");
-    }
-    if (len_bytes > MAX_BPF_SIZE){
-        die("len_bytes is larger than MAX_BPF_SIZE not allowed");
-    }
-    if (len_bytes % sizeof(struct sock_filter) != 0){
-        die("filter length %u is not a multiple of instruction size %u", len_bytes, sizeof(struct sock_filter));
-    }
+        if (len_bytes == 0){
+            die("len_bytes can not be 0");
+        }
+        if (len_bytes > MAX_BPF_SIZE){
+            die("len_bytes is larger than MAX_BPF_SIZE not allowed");
+        }
+        if (len_bytes % sizeof(struct sock_filter) != 0){
+            die("filter length %u is not a multiple of instruction size %u", len_bytes, sizeof(struct sock_filter));
+        }
 
 	prog->len = len_bytes / sizeof(struct sock_filter);
 	prog->filter = (struct sock_filter *)malloc(len_bytes);
